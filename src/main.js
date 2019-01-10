07befa41fceb5f4e31eb71f96b62543e853440ce
SmaVivian <vivi163abc@163.com>
2019-01-10 09:30:57 +0800

    添加富文本编辑器，添加表单Demo

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
+import VueQuillEditor  from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+ 
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -15,7 +20,9 @@ import store from './store'
 import '@/icons' // icon
 import '@/permission' // permission control
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI)
+Vue.use(VueQuillEditor);
+// Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -4,12 +4,28 @@
       <el-form-item label="Activity name">
         <el-input v-model="form.name"/>
       </el-form-item>
+
+      <!-- 下拉框静态 -->
       <el-form-item label="Activity zone">
         <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai"/>
-          <el-option label="Zone two" value="beijing"/>
+          <el-option label="上海" value="shanghai"/>
+          <el-option label="北京" value="beijing"/>
         </el-select>
       </el-form-item>
+
+      <!-- 下拉框动态渲染 -->
+      <el-form-item label="Activity zone2">
+        <el-select v-model="form.value" placeholder="请选择">
+          <el-option
+            v-for="item in form.options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <!-- 日期/时间 -->
       <el-form-item label="Activity time">
         <el-col :span="11">
           <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;"/>
@@ -19,9 +35,29 @@
           <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;"/>
         </el-col>
       </el-form-item>
+
+      <!-- switch开关 -->
       <el-form-item label="Instant delivery">
         <el-switch v-model="form.delivery"/>
       </el-form-item>
+
+      <!-- 单选框 -->
+      <el-form-item label="Resources">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="Sponsor"/>
+          <el-radio label="Venue"/>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="单选框">
+        <el-radio-group v-model="form.radio">
+          <el-radio 
+            v-for="item in radioList"
+            :key="item.value"
+            :label="item.value">{{item.label}}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 复选框 -->
       <el-form-item label="Activity type">
         <el-checkbox-group v-model="form.type">
           <el-checkbox label="Online activities" name="type"/>
@@ -30,15 +66,33 @@
           <el-checkbox label="Simple brand exposure" name="type"/>
         </el-checkbox-group>
       </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor"/>
-          <el-radio label="Venue"/>
-        </el-radio-group>
-      </el-form-item>
+
+      <!-- 文本域 -->
       <el-form-item label="Activity form">
         <el-input v-model="form.desc" type="textarea"/>
       </el-form-item>
+
+      <!-- 富文本 -->
+      <!-- bidirectional data binding（双向数据绑定） -->
+      <el-form-item label="富文本1" style="height:300px;">
+        <quill-editor v-model="content"
+          ref="myQuillEditor"
+          :options="editorOption"
+          @blur="onEditorBlur($event)"
+          @focus="onEditorFocus($event)"
+          @ready="onEditorReady($event)"
+           style="height:200px;">
+        </quill-editor>
+      </el-form-item>
+    
+      <!-- Or manually control the data synchronization（或手动控制数据流） -->
+      <el-form-item label="富文本2">
+        <quill-editor :content="content2"
+          :options="editorOption"
+          @change="onEditorChange($event)">
+        </quill-editor>
+      </el-form-item>
+
       <el-form-item>
         <el-button type="primary" @click="onSubmit">Create</el-button>
         <el-button @click="onCancel">Cancel</el-button>
@@ -51,16 +105,58 @@
 export default {
   data() {
     return {
+      radioList: [
+        {
+          value: '1',
+          label: '备选项'
+        },
+        {
+          value: '2',
+          label: '备选项2'
+        },
+        {
+          value: '3',
+          label: '备选项3'
+        },
+      ],
+      content: '<h2>I am Example</h2>',
+      content2: '<h2>I am Example</h2>',
+      editorOption: {
+        // some quill options
+      },
       form: {
         name: '',
         region: '',
+        options: [{
+          value: '',
+          label: '全部'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: '',
         date1: '',
         date2: '',
         delivery: false,
+        radio: '2',
         type: [],
         resource: '',
         desc: ''
       }
+    }
+  },
+  computed: {
+    editor() {
+      return this.$refs.myQuillEditor.quill
     }
   },
   methods: {
@@ -72,7 +168,27 @@ export default {
         message: 'cancel!',
         type: 'warning'
       })
+    },
+
+    onEditorBlur(quill) {
+      console.log('editor blur!', quill)
+    },
+    onEditorFocus(quill) {
+      console.log('editor focus!', quill)
+    },
+    onEditorReady(quill) {
+      console.log('editor ready!', quill)
+    },
+    onEditorChange({ quill, html, text }) {
+      console.log('editor change!', quill, html, text)
+      this.content = html
     }
+  },
+  mounted() {
+    console.log('this is current quill instance object', this.editor)
+    setTimeout(() => {
+      this.content = '<p>我修改啦</p>'
+    }, 2000)
   }
 }
 </script>
