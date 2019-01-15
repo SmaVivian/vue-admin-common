fd7843eebe5f0976a13f7dfdb662bef0863b17ff
SmaVivian <vivi163abc@163.com>
2019-01-15 17:52:58 +0800

    添加全局缓存

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,39 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+//使用Vue.mixin的方法拦截了路由离开事件，并在该拦截方法中实现了销毁页面缓存的功能。
+Vue.mixin({
+  beforeRouteLeave:function(to, from, next){
+    //此处判断是如果返回上一层，你可以根据自己的业务更改此处的判断逻辑，酌情决定是否摧毁本层缓存。
+    if (from && from.meta.rank && to.meta.rank && from.meta.rank>to.meta.rank) {
+      
+      if (this.$vnode && this.$vnode.data.keepAlive) {
+        if (this.$vnode.parent && this.$vnode.parent.componentInstance && this.$vnode.parent.componentInstance.cache) {
+          if (this.$vnode.componentOptions)
+          {
+            var key = this.$vnode.key == null
+              ? this.$vnode.componentOptions.Ctor.cid + (this.$vnode.componentOptions.tag ? `::${this.$vnode.componentOptions.tag}` : '')
+              : this.$vnode.key;
+            var cache = this.$vnode.parent.componentInstance.cache;
+            var keys  = this.$vnode.parent.componentInstance.keys;
+            if (cache[key]) {
+              if (keys.length) {
+                var index = keys.indexOf(key);
+                if (index > -1) {
+                  keys.splice(index, 1);
+                }
+              }
+              delete cache[key];
+            }
+          }
+        }
+      }
+      this.$destroy();
+    }
+    next();
+  },
+});
+
 /* Layout */
 import Layout from '../views/layout/Layout'
 
@@ -51,20 +84,20 @@ export const asyncRouterMap = [
         path: 'index',
         name: 'ListIndex',
         component: () => import('@/views/list/index'),
-        meta: { title: '列表', icon: 'table' }
+        meta: { title: '列表', icon: 'table', rank: 1 },
       },
       {
         path: 'detail',
         name: 'ListDetail',
         component: () => import('@/views/list/detail'),
-        meta: { title: '详情', icon: 'form' },
+        meta: { title: '详情', icon: 'form', rank: 2 },
         hidden: true
       },
       {
         path: 'edit',
         name: 'ListEdit',
         component: () => import('@/views/list/edit'),
-        meta: { title: '编辑', icon: 'form' },
+        meta: { title: '编辑', icon: 'form', rank: 2 },
         hidden: true
       },
     ]
